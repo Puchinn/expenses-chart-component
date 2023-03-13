0a44bc0552cc105016bc477d0b769190aaac7e26
Esteban Sayago <sayagoo823@gmail.com>
2023-03-13 19:52:18 -0300

    update component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Balance from './components/Balance'
+import Chart from './components/Chart'
+
 function App() {
 	return (
-		<div className='App'>
-			<h1 className='text-5xl text-red-600'>Hola mundinho</h1>
+		<div className='App flex h-screen items-center justify-center bg-Cream'>
+			<div className='w-full max-w-lg space-y-6'>
+				<Balance />
+				<Chart />
+			</div>
 		</div>
 	)
 }
