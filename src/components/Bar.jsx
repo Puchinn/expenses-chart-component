a6b022948aafbd120e4d91b13d496a19e5c15f16
Esteban Sayago <sayagoo823@gmail.com>
2023-03-13 19:55:46 -0300

    fix styles

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -1,12 +1,13 @@
 function Bar({ day, amount }) {
-	const altura = Math.round(amount) * 3
+	const altura = Math.round(amount) * 3.5
 	return (
 		<div className='group relative w-min cursor-pointer space-y-3'>
 			<div className='absolute -top-9 -left-3 hidden w-min rounded-md bg-Dark-brown py-2 px-3 font-semibold tracking-wide group-hover:block'>
 				<p className='text-Very-pale-orange'>{amount}</p>
 			</div>
 			<div
-				className={`mx-auto h-[${altura}px] w-12 rounded-md bg-Soft-red group-hover:bg-Cyan`}></div>
+				className='mx-auto w-12 rounded-md bg-Soft-red group-hover:bg-Cyan'
+				style={{ height: altura }}></div>
 			<p className='text-center text-Medium-brown'>{day}</p>
 		</div>
 	)
