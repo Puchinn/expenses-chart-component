6cd9dbac61e15724243b128af3a49e3b2a01ea49
Esteban Sayago <sayagoo823@gmail.com>
2023-03-13 22:22:33 -0300

    use useEffect to set the day

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,13 +1,18 @@
 import Bar from './Bar'
 import data from '../data'
+import { useState, useEffect } from 'react'
 
 function Chart() {
-	let hoy = new Date().getDay()
-	if (hoy - 1 < 0) {
-		hoy = data.length - 1
-	} else {
-		hoy--
-	}
+	const [today, setToday] = useState(null)
+	useEffect(() => {
+		const hoy = new Date().getDay()
+		if (hoy - 1 < 0) {
+			setToday(data.length - 1)
+		} else {
+			setToday(hoy - 1)
+		}
+	}, [])
+
 	return (
 		<div className='space-y-6 rounded-2xl bg-Very-pale-orange px-9 pt-7 pb-12'>
 			<h1 className='text-3xl text-Dark-brown'>
@@ -15,7 +20,7 @@ function Chart() {
 			</h1>
 			<div className='flex items-end justify-around gap-x-3 border-b-2 border-Cream pt-8 pb-7'>
 				{data.map((item, i) => {
-					if (i === hoy) {
+					if (i === today) {
 						return <Bar key={item.day} day={item.day} amount={item.amount} today={true} />
 					}
 					return <Bar key={item.day} day={item.day} amount={item.amount} today={false} />
